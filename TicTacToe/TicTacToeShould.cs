567f434a1f0e11b0518d7bb34963a4ab62e04c9a
Leandro Cieri <lcieri@domingoalonsogroup.com>
2023-01-24 11:01:45 +0000

    Refactor

--- TicTacToe/TicTacToeShould.cs
+++ TicTacToe/TicTacToeShould.cs
@@ -5,58 +5,76 @@ namespace TicTacToe
     public class TicTacToeShould
     {
         [Test]
-        public void throw_if_starting_player_is_not_X()
+        public void Throw_if_starting_player_is_not_X()
         {
             var game = new Game();
 
-            var play = () => game.Play(Player.O, 0, 0);
+            var play = () => game.Play(Player.O, new Position(0, 0));
 
             play.Should().Throw<WrongTurnException>();
         }
 
         [Test]
-        public void not_throw_if_starting_player_is_X()
+        public void Not_throw_if_starting_player_is_X()
         {
             var game = new Game();
 
-            var play = () => game.Play(Player.X, 0, 0);
+            var play = () => game.Play(Player.X, new Position(0, 0));
 
             play.Should().NotThrow<WrongTurnException>();
         }
 
         [Test]
-        public void alternate_turns_between_player_X_and_O()
+        public void Alternate_turns_between_player_X_and_O()
         {
             var game = new Game();
-            game.Play(Player.X, 0, 1);
+            game.Play(Player.X, new Position(0, 1));
 
-            var play = () => game.Play(Player.X, 0, 0);
+            var play = () => game.Play(Player.X, new Position(0, 0));
 
             play.Should().Throw<WrongTurnException>();
         }
 
         [Test]
-        public void not_be_able_to_play_same_position_twice()
+        public void Not_be_able_to_play_same_position_twice()
         {
             var game = new Game();
-            game.Play(Player.X, 0, 0);
+            game.Play(Player.X, new Position(0, 0));
 
-            var play = () => game.Play(Player.O, 0, 0);
+            var play = () => game.Play(Player.O, new Position(0, 0));
 
             play.Should().Throw<PlayedPositionIsNotEmptyException>();
+        }
+
+        [Test]
+        public void Declare_a_winner_if_there_is_a_row_with_the_same_player()
+        {
+            var game = new Game();
+            game.Play(Player.X, new Position(0, 0));
+            game.Play(Player.O, new Position(1, 0));
+            game.Play(Player.X, new Position(0, 1));
+            game.Play(Player.O, new Position(1, 1));
+
+            var play = () => game.Play(Player.X, new Position(0, 2));
         }
     }
 
     public class Game
     {
         private Player _lastPlayer = Player.O;
-        private Board _board = new();
+        private readonly Board _board = new();
 
-        public void Play(Player player, int x, int y)
+        public void Play(Player player, Position position)
+        {
+            CheckTurns(player);
+            _board.SetPosition(player, position);
+
+        }
+
+        private void CheckTurns(Player player)
         {
             if (_lastPlayer == player)
                 throw new WrongTurnException();
-            _board.SetPosition(x, y, player);
             _lastPlayer = player;
         }
     }
@@ -65,11 +83,25 @@ namespace TicTacToe
     {
         private readonly string[,] _cells = new string[3, 3];
 
-        public void SetPosition(int x, int y, Player player)
+        public void SetPosition(Player player, Position position)
         {
-            if (!string.IsNullOrEmpty(_cells[x, y]))
+            if (CellIsOccupied(position))
                 throw new PlayedPositionIsNotEmptyException();
-            _cells[x, y] = player.ToString();
+            _cells[position.X, position.Y] = player.ToString();
+        }
+
+        private bool CellIsOccupied(Position position) => !string.IsNullOrEmpty(_cells[position.X, position.Y]);
+    }
+
+    public class Position
+    {
+        public int X;
+        public int Y;
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
         }
     }
 
